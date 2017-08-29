6e770ae9dd7ed8579adb61adbc5865a17f322888
Beer <wolfgang.beer@dynatrace.com>
2017-08-29 07:08:22 +0200

    added book ratings file

--- tensorflow/books.py
+++ tensorflow/books.py
@@ -1,11 +1,11 @@
-# books are 1-10000, for users, 1-53424
+# books are 1-8, for users, 1-6
 # rating from 1 to 5
-import numpy
+import numpy as np
 import tensorflow as tf
 import pandas as pd
 	
-'''
-BOOKS = numpy.zeros(shape=(10000,53424), dtype=float)	
+
+BOOKS = np.zeros(shape=(8,6), dtype=float)
 
 CSV_COLUMNS = [
 		"book_id","user_id","rating"
@@ -16,23 +16,14 @@ print("Book ratings read")
 for index, row in data.iterrows():
 	BOOKS[int(row['book_id'])-1][int(row['user_id'])-1] = int(row['rating']);
 print("Book ratings array created")	
-'''
 
-BOOKS = [ # columns = user, rows = books
-     [4,0,0,0,2,0],
-     [0,0,0,3,5,0],
-     [0,1,0,5,4,0],
-     [1,4,0,0,0,5],
-     [0,5,0,0,0,0],
-    ]
-	
 # prepare data
-R = numpy.array(BOOKS)
+R = np.array(BOOKS)
 N = len(BOOKS)
 M = len(BOOKS[0])
 K = 2 # number of hidden features
-P = numpy.random.rand(N,K)
-Q = numpy.random.rand(M,K)
+P = np.random.rand(N,K)
+Q = np.random.rand(M,K)
 
 # input placeholders  
 ratings = tf.placeholder(tf.float32, name = 'ratings')
@@ -42,13 +33,14 @@ tP = tf.Variable(P, dtype=tf.float32, name='P')
 tQ = tf.Variable(Q, dtype=tf.float32, name='Q')
   
 # build model
-pDotq = tf.tensordot(tP, tQ, [[1], [1]]) 
+pMultq = tf.matmul(tP, tQ, transpose_b=True)
   
-squared_deltas = tf.square(pDotq - ratings)
+squared_deltas = tf.square(pMultq - ratings)
 loss = tf.reduce_sum(squared_deltas)
 tf.summary.scalar('loss', loss)
 tf.summary.scalar('sumP', tf.reduce_sum(tP))
 tf.summary.scalar('sumQ', tf.reduce_sum(tQ))
+
    
 # create an optimizer towards minimizing the loss value
 optimizer = tf.train.GradientDescentOptimizer(0.01)
@@ -63,13 +55,14 @@ summary_op = tf.summary.merge_all()
 file_writer = tf.summary.FileWriter('./logs', sess.graph)
 	
 # now run the training loop to reduce the loss
-for i in range(100):
+for i in range(5000):
     # also execute a summary operation together with the train node for TensorBoard
-    _, summary = sess.run([train, summary_op], {ratings: BOOKS})
+    _, summary = sess.run([train, summary_op], {ratings: R})
     file_writer.add_summary(summary, i)
     print(i)
-  
-print(sess.run(pDotq))
+
+
+print(np.around(sess.run(pMultq),3))
 
 
 
